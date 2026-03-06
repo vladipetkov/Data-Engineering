ae19d94045d487f5b919557eb6776f151495dab3
MarkNemeth05 <music.m.spoty@gmail.com>
2026-03-06 14:36:34 +0100

    Revert "Merge branch 'main' into mark"
    This reverts commit 3fdac5a33470d1efc1f836805f20a5b5ba06f6b2, reversing
    changes made to c0a7de66220f0ac17df71abb4a5284d802aa97b6.

--- code/part_3.py
+++ code/part_3.py
@@ -2,7 +2,6 @@ import sqlite3
 import pandas as pd
 from typing import Dict, List
 
-import matplotlib.pyplot as plt
 
 con = sqlite3.connect("spotify_database.db")
 cur = con.cursor()
@@ -142,41 +141,3 @@ def map_broad_genres(con, genre_keywords: Dict[str, List[str]] = GENRE_KEYWORDS)
 
 df = map_broad_genres(con)
 print(df[["name", "broad_genres"]].head(50).to_string(index=False))
-
-
-
-"""
-ex. 5 Are explicit tracks more popular?
-"""
-explicit_popularity = []
-nonexplicit_popularity = []
-
-cur.execute("select track_popularity from tracks_data where explicit = 'true';")
-rows = cur.fetchall()
-
-for row in rows:
-    explicit_popularity.append(row[0])
-
-cur.execute("select track_popularity from tracks_data where explicit = 'false';")
-rows = cur.fetchall()
-
-for row in rows:
-    nonexplicit_popularity.append(row[0])
-
-plt.boxplot([explicit_popularity, nonexplicit_popularity], tick_labels=["Explicit Content", "Non-Explicit Content"])
-plt.ylabel("Popularity Score")
-plt.title("Popularity of Explicit vs Non-Explicit Content")
-plt.show()
-
-"""
-ex. 6 Which artists have the highest proportion of explicit tracks?
-"""
-query = "select ar.name, count(*) from artist_data ar join albums_data al on ar.id = al.artist_id join tracks_data t on al.track_id = t.id " \
-"where t.explicit = 'true' group by ar.name order by count(*) desc limit 10;"
-
-cur.execute(query)
-top_10_explicit_artists = cur.fetchall()
-
-print("artist             songs \n------------------------")
-for artist in top_10_explicit_artists:
-    print(f"{artist[0]:20} {artist[1]}")
