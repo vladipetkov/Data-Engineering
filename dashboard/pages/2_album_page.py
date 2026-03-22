28eda5118dc772f029721a070e60762c17fb0025
Vlad <118540746+vladipetkov@users.noreply.github.com>
2026-03-23 00:44:02 +0100

    Update 2_album_page.py
    update on paths

--- dashboard/pages/2_album_page.py
+++ dashboard/pages/2_album_page.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 logo_path = Path(__file__).parent.parent / "supporting" / "logo.png"
 st.set_page_config(page_title="Album Lookup", layout="wide")
-st.logo("supporting/logo.png", size = "large")
+st.logo(logo_path, size = "large")
 
 with st.sidebar:
     selectbox_option = st.sidebar.selectbox(
@@ -22,16 +22,16 @@ with st.sidebar:
     artist_page_path = Path(__file__).parent / "1_artist_page.py"
     album_page_path = Path(__file__).parent / "2_album_page.py"
     if selectbox_option == "Look up an Artist":
-        st.switch_page("pages/1_artist_page.py")
+        st.switch_page(artist_page_path)
     elif selectbox_option == "Look up an Album or a Track":
-        st.switch_page("pages/2_album_page.py")
+        st.switch_page(album_page_path)
 
     st.space(500)
     
     event_planning_path = Path(__file__).parent / "4_event_planning.py"
     if st.sidebar.button("Business Tab" , type = "primary", width = "stretch"):
         st.session_state["play_event_planning_intro"] = True
-        st.switch_page("pages/4_event_planning.py")
+        st.switch_page(event_planning_path)
 
 conn = get_connection()
 cursor = conn.cursor()
