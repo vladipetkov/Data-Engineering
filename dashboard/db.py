23f7f825c8f99561220b7019ce1e1c8e59a9b773
Vlad <118540746+vladipetkov@users.noreply.github.com>
2026-03-23 00:28:57 +0100

    Update db.py
    path update

--- dashboard/db.py
+++ dashboard/db.py
@@ -1,6 +1,9 @@
 import sqlite3
 import streamlit as st
+from pathlib import Path
+
+db_path = Path(__file__).parent /"spotify_database.db"
 
 @st.cache_resource
 def get_connection():
-    return sqlite3.connect("spotify_database.db", check_same_thread=False)
+    return sqlite3.connect(db_path, check_same_thread=False)
